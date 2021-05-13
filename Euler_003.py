257a94aa2cbde456018c13e8a8653eecdacde62f
cavandervoort <61097283+cavandervoort@users.noreply.github.com>
2021-05-13 16:21:29 -0700

    Update Euler_003.py

--- Euler_003.py
+++ Euler_003.py
@@ -6,11 +6,13 @@ bigNum = 600851475143
 largestFactor = 1
 
 for num in range(3,100000,2):
+    if num > bigNum ** 0.5:
+        break
+
     for prime in primes:
         if num % prime == 0:
             break
         elif prime > num ** 0.5:
-            print(f'testing prime of {num}')
             primes.append(num)
             if bigNum % num == 0:
                 largestFactor = num
@@ -21,4 +23,4 @@ for num in range(3,100000,2):
     if bigNum == 1:
         break
         
-print(largestFactor)
+print(int(max(largestFactor,bigNum)))
